85c1c8839c28413d0b53c8a93b2fa5b9855f88a1
rizki <rizkiadnanfutuh@gmail.com>
2024-10-11 10:40:27 +0700

    feat: create homescreen

---
+++ lib/features/home/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Column(
+        children: [
+          Center(child: Text('Banner')),
+          SizedBox(height: 100),
+          Center(child: Text('Feature Widget')),
+          SizedBox(height: 100),
+          Center(child: Text(' Sholat Time Widget')),
+          Spacer(),
+          Center(child: Text('Bottom Navigation')),
+        ],
+      ),
+    );
+  }
+}
