10001524a8129981715679221c67f319c1bc67e9
rizki <rizkiadnanfutuh@gmail.com>
2024-10-25 23:27:08 +0700

    refactor

--- lib/presentation/widgets/bottom_nav_widget.dart
+++ lib/presentation/widgets/bottom_nav_widget.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../features/book_mark/screens/book_mark_screen.dart';
+import '../../presentation/cubit/bottomnav_cubit.dart';
 import '../../features/home/screens/home_screen.dart';
+import '../../features/book_mark/screens/book_mark_screen.dart';
 import '../../features/prayer_time/screens/prayer_time_screen.dart';
 import '../../features/profile/screens/profile_screen.dart';
-import '../cubit/bottomnav_cubit.dart';
 
 class BottomNavWidget extends StatelessWidget {
   const BottomNavWidget({super.key});
@@ -31,25 +30,33 @@ class BottomNavWidget extends StatelessWidget {
           );
         },
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.cyan,
-        unselectedItemColor: Colors.grey,
-        currentIndex: context.read<BottomNavCubit>().state.map(
-              initial: (_) => 0,
-              changed: (changed) => changed.index,
-            ),
-        onTap: (index) {
-          context.read<BottomNavCubit>().changeTab(index);
+      bottomNavigationBar: BlocBuilder<BottomNavCubit, BottomNavState>(
+        builder: (context, state) {
+          final currentIndex = state.map(
+            initial: (_) => 0,
+            changed: (changed) => changed.index,
+          );
+
+          return BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.cyan,
+            unselectedItemColor: Colors.grey,
+            backgroundColor: Colors.black,
+            currentIndex: currentIndex,
+            onTap: (index) {
+              context.read<BottomNavCubit>().changeTab(index);
+            },
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.bookmark), label: 'Bookmark'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.access_time), label: 'Jadwal Sholat'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person), label: 'Profile'),
+            ],
+          );
         },
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.bookmark), label: 'Bookmark'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.access_time), label: 'Jadwal Sholat'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
       ),
     );
   }
