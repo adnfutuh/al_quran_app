b00ac61a6d0eb972f5611a002ce55d277b2ae02d
rizki <rizkiadnanfutuh@gmail.com>
2024-11-06 22:59:33 +0700

    feat: add ShellRoute

--- lib/core/routes/router_config.dart
+++ lib/core/routes/router_config.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
-
+import 'package:flutter/material.dart';
 import '../../features/listquran/presentation/ui/screens/list_quran_sccreen.dart';
 import '../../features/navbar/presentation/ui/screens/book_mark/screens/book_mark_screen.dart';
 import '../../features/navbar/presentation/ui/screens/home/presentation/ui/screens/home_screen.dart';
@@ -10,27 +10,35 @@ import '../../features/navbar/presentation/ui/screens/profile/screens/profile_sc
 
 class RoutesConfig {
   static final GoRouter appRouter = GoRouter(
-    initialLocation: '/navbar',
+    initialLocation: '/navbar/home',
     routes: [
-      GoRoute(
-        path: '/navbar',
-        builder: (context, state) => const BottomNavWidget(),
+      ShellRoute(
+        navigatorKey: GlobalKey<NavigatorState>(),
+        builder: (context, state, child) {
+          return BottomNavWidget(child: child);
+        },
         routes: [
           GoRoute(
-            path: 'home',
+            path: '/navbar',
             builder: (context, state) => const HomeScreen(),
-          ),
-          GoRoute(
-            path: 'bookmark',
-            builder: (context, state) => const BookMarkScreen(),
-          ),
-          GoRoute(
-            path: 'pryertime',
-            builder: (context, state) => const PrayerTimeScreen(),
-          ),
-          GoRoute(
-            path: 'profile',
-            builder: (context, state) => const ProfileScreen(),
+            routes: [
+              GoRoute(
+                path: 'home',
+                builder: (context, state) => const HomeScreen(),
+              ),
+              GoRoute(
+                path: 'bookmark',
+                builder: (context, state) => const BookMarkScreen(),
+              ),
+              GoRoute(
+                path: 'pryertime',
+                builder: (context, state) => const PrayerTimeScreen(),
+              ),
+              GoRoute(
+                path: 'profile',
+                builder: (context, state) => const ProfileScreen(),
+              ),
+            ],
           ),
         ],
       ),
@@ -44,7 +52,7 @@ class RoutesConfig {
           final id = int.tryParse(state.pathParameters['id'] ?? '0') ?? 0;
           return QuranScreen(surahId: id);
         },
-      )
+      ),
     ],
   );
 }
