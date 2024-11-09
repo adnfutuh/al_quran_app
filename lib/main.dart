524e68dd268a5a9e4d90305f325d48f39cbf26cf
rizki <rizkiadnanfutuh@gmail.com>
2024-11-09 15:43:19 +0700

    feat: add BlocProvider for AudioCubit

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
 
+import 'features/audio/presentation/cubit/audio_cubit.dart';
 import 'features/listquran/presentation/cubit/listquran_cubit.dart';
 import 'features/navbar/presentation/cubit/bottomnav_cubit.dart';
 import 'features/navbar/presentation/ui/screens/home/presentation/cubit/location_cubit.dart';
 import 'features/prayer/presentation/cubit/prayer_cubit.dart';
+import 'features/quran/presentation/cubit/quran_cubit.dart';
 
 void main() async {
   await ScreenUtil.ensureScreenSize();
@@ -17,31 +19,40 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(375, 812),
-        child: MultiBlocProvider(
-          providers: [
-            BlocProvider(
-              create: (context) => GetIt.I<ListquranCubit>(),
-            ),
-            BlocProvider(
-              create: (context) => GetIt.I<LocationCubit>()..fetchLocation(),
-            ),
-            BlocProvider(
-              create: (context) => GetIt.I<PrayerCubit>()..fetchPrayerTime(),
-            ),
-            BlocProvider(
-              create: (context) => BottomNavCubit(),
-            )
-          ],
-          child: MaterialApp.router(
-            debugShowCheckedModeBanner: false,
-            routerConfig: RoutesConfig.appRouter,
+      designSize: const Size(375, 812),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => GetIt.I<ListquranCubit>()..getListQuranData(),
           ),
-        ));
+          BlocProvider(
+            create: (context) => GetIt.I<QuranCubit>(),
+          ),
+          BlocProvider(
+            create: (context) => GetIt.I<LocationCubit>()..fetchLocation(),
+          ),
+          BlocProvider(
+            create: (context) => GetIt.I<PrayerCubit>()..fetchPrayerTime(),
+          ),
+          BlocProvider(
+            create: (context) => BottomNavCubit(),
+          ),
+          BlocProvider(
+            create: (context) => AudioCubit(),
+          ),
+        ],
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          routerConfig: RoutesConfig.appRouter,
+        ),
+      ),
+    );
   }
 }
