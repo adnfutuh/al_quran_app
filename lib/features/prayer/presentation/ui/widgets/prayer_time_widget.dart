39fbea7065f98c6f5cb8affcc359ba2f7b64194b
rizki <rizkiadnanfutuh@gmail.com>
2024-11-12 18:43:52 +0700

    feat: add icon

--- lib/features/prayer/presentation/ui/widgets/prayer_time_widget.dart
+++ lib/features/prayer/presentation/ui/widgets/prayer_time_widget.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:flutter/cupertino.dart';
 import '../../../../../core/core.dart';
 import '../../cubit/prayer_cubit.dart';
 
@@ -17,7 +18,6 @@ class _PrayerTimeWidgetState extends State<PrayerTimeWidget> {
   void initState() {
     super.initState();
     final cubit = GetIt.I<PrayerCubit>();
-
     cubit.fetchPrayerTime();
   }
 
@@ -56,22 +56,48 @@ class _PrayerTimeWidgetState extends State<PrayerTimeWidget> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _buildPrayerTimeColumn('Fajr', prayerTime.data.timings.Fajr),
-        _buildPrayerTimeColumn('Dhuhr', prayerTime.data.timings.Dhuhr),
-        _buildPrayerTimeColumn('Asr', prayerTime.data.timings.Asr),
-        _buildPrayerTimeColumn('Maghrib', prayerTime.data.timings.Maghrib),
-        _buildPrayerTimeColumn('Isha', prayerTime.data.timings.Isha),
+        _buildPrayerTimeColumn(
+          'Fajr',
+          CupertinoIcons.cloud_sun_fill,
+          prayerTime.data.timings.Fajr,
+        ),
+        _buildPrayerTimeColumn(
+          'Dhuhr',
+          CupertinoIcons.sun_max_fill,
+          prayerTime.data.timings.Dhuhr,
+        ),
+        _buildPrayerTimeColumn(
+          'Asr',
+          CupertinoIcons.sun_haze_fill,
+          prayerTime.data.timings.Asr,
+        ),
+        _buildPrayerTimeColumn(
+          'Maghrib',
+          CupertinoIcons.cloud_moon_fill,
+          prayerTime.data.timings.Maghrib,
+        ),
+        _buildPrayerTimeColumn(
+          'Isha',
+          CupertinoIcons.moon_stars_fill,
+          prayerTime.data.timings.Isha,
+        ),
       ],
     );
   }
 
-  Widget _buildPrayerTimeColumn(String title, String time) {
+  Widget _buildPrayerTimeColumn(String title, IconData icon, String time) {
     return Column(
       children: [
         Text(
           title,
           style: TextStyles.textSmRegular.copyWith(color: Pallet.white),
         ),
+        const SizedBox(height: 4),
+        Icon(
+          icon,
+          color: Pallet.white,
+        ),
+        const SizedBox(height: 4),
         Text(
           time,
           style: TextStyles.textSmRegular.copyWith(color: Pallet.white),
